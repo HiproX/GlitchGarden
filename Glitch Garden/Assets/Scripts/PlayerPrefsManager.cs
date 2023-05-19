d74e42faaee633cfdc09f07ec37cb4f50c015cb9
Eugene <anapchy@mail.ru>
2023-05-19 21:20:42 +0700

    Our PlayerPrefs manager created
    - the PlayerPrefsManager wrapper was created over PlayerPrefs

---
+++ Glitch Garden/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1.0f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Громкость вне диапазона");
+        }
+    }
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+    public static void UnlockLevel(int level)
+    {
+        if (level >= 0 && level <= SceneManager.sceneCount - 1)
+        {
+            PlayerPrefs.SetInt($"{LEVEL_KEY}{level}", 1); // 1 == true
+        }
+        else
+        {
+            Debug.LogError("Попытка разблокировки некоректного уровня");
+        }
+    }
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level < 0 || level > SceneManager.sceneCount - 1)
+        {
+            Debug.LogError("Попытка запросить некоректный уровень");
+        }
+        return level >= 0 && level <= SceneManager.sceneCount - 1 &&
+            PlayerPrefs.GetInt($"{LEVEL_KEY}{level}") == 1;
+    }
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty >= 0f && difficulty <= 1.0f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Сложность вне диапазано");
+        }
+    }
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
